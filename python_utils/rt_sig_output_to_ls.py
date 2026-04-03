e49a0b4a4d685d37d8c6617e583893f69b5830cc
Eli Goldner <etgld@posteo.us>
2026-04-03 11:01:47 -0400

    Some tweaks

--- python_utils/rt_sig_output_to_ls.py
+++ python_utils/rt_sig_output_to_ls.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from typing import Any
+from frozendict import frozendict
 from more_itertools import map_reduce, one
 from enum import Enum
 from itertools import chain
@@ -8,7 +10,15 @@ import json
 import os
 from functools import cache, partial
 import polars as pl
+import logging
 
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 parser = argparse.ArgumentParser(description="")
 
 parser.add_argument(
@@ -74,18 +84,24 @@ def ctakes_csv_to_ls_file_annotation(
         else None
     )
     rt_annotations = (
-        chain.from_iterable(
-            rt_dict_to_ls_annotations(
-                row_dict=row_dict,
-                file_index=file_index,
-                row_index=row_index,
-                file_text=file_text,
-            )
-            for row_index, row_dict in enumerate(rt_frame.to_dicts())
+        rt_frame_to_ls_annotations(
+            rt_frame=rt_frame, file_index=file_index, file_text=file_text
         )
         if rt_frame is not None
         else []
-    )
+    )  # (
+    #     chain.from_iterable(
+    #         rt_dict_to_ls_annotations(
+    #             row_dict=row_dict,
+    #             file_index=file_index,
+    #             row_index=row_index,
+    #             file_text=file_text,
+    #         )
+    #         for row_index, row_dict in enumerate(rt_frame.to_dicts())
+    #     )
+    #     if rt_frame is not None
+    #     else []
+    # )
 
     ae_annotations = (
         chain.from_iterable(
@@ -254,15 +270,19 @@ def cui_cell_to_ls_entity(
     cui_labels: list[str],
     ls_id: str,
     origin: str,
-) -> dict:
-    return {
-        "value": {"start": start, "end": end, "text": cui_labels},
-        "id": ls_id,
-        "from_name": "CUI",
-        "to_name": "text",
-        "type": "textarea",
-        "origin": origin,
-    }
+) -> frozendict:
+    return frozendict(
+        {
+            "value": frozendict(
+                {"start": start, "end": end, "text": tuple(sorted(cui_labels))}
+            ),
+            "id": ls_id,
+            "from_name": "CUI",
+            "to_name": "text",
+            "type": "textarea",
+            "origin": origin,
+        }
+    )
 
 
 def ae_cell_to_ls_entity(
@@ -313,20 +333,24 @@ def cell_to_ls_entity(
     from_name: str,
     to_name: str,
     origin: str,
-) -> dict:
-    return {
-        "value": {
-            "start": start,
-            "end": end,
-            "text": text,
-            entity_type: entity_labels,
-        },
-        "id": ls_id,
-        "from_name": from_name,
-        "to_name": to_name,
-        "type": entity_type,
-        "origin": origin,
-    }
+) -> frozendict:
+    return frozendict(
+        {
+            "value": frozendict(
+                {
+                    "start": start,
+                    "end": end,
+                    "text": text,
+                    entity_type: tuple(sorted(entity_labels)),
+                }
+            ),
+            "id": ls_id,
+            "from_name": from_name,
+            "to_name": to_name,
+            "type": entity_type,
+            "origin": origin,
+        }
+    )
 
 
 def ae_dict_to_ls_annotations(
@@ -376,6 +400,197 @@ def ae_dict_to_ls_annotations(
         ls_id=local_build_id(annotation_index=current),
         origin="prediction",
     )
+
+
+def rt_frame_to_ls_annotations(
+    rt_frame: pl.DataFrame, file_index: int, file_text: str
+) -> Iterable[dict]:
+    center_dose_to_signatures = {}
+    non_dose_attr_columns = set(rt_frame.columns) - {"dtr", "cuis"}
+    for (offset_str,), sub_table in rt_frame.group_by("central_dose"):
+        result = {}
+        dtrs = set(sub_table["dtr"].to_list())
+        try:
+            result["dtr"] = one(dtrs)
+        except Exception:
+            ValueError(f"Inconsistent dtr values {dtrs}")
+        cuis = set(sub_table["cuis"].to_list())
+        try:
+            raw = one(cuis)
+            result["cuis"] = (
+                ["C1522449"] if raw is None or raw != "None" else raw.split("_")
+            )
+        except Exception:
+            ValueError(f"Inconsistent cuis values {cuis}")
+        center_dose_offsets = parse_offset_str(offset_str)
+        if center_dose_offsets is None:
+            raise ValueError(f"Malformed center dose offsets: {offset_str}")
+        for column in non_dose_attr_columns:
+            vals = {
+                parse_offset_str(val)
+                for val in set(sub_table[column].to_list())
+                if val != "None" and val is not None
+            }
+            try:
+                offsets = one(
+                    vals,
+                    too_long=ValueError,
+                    too_short=IndexError,
+                )
+            except ValueError:
+                logger.error(
+                    "Bad values for column %s: %s - Selecting element closest to center dose",
+                    column,
+                    ", ".join(map(str, sorted(vals))),
+                )
+                offsets = min(vals, key=lambda s: abs(s[0] - center_dose_offsets[0]))
+            except IndexError:
+                offsets = None
+            if offsets is not None:
+                result[column] = offsets
+        center_dose_to_signatures[center_dose_offsets] = result
+    for signatures in center_dose_to_signatures.values():
+        secondary_dose = signatures.get("secondary_dose")
+        if (
+            secondary_dose is not None
+            and secondary_dose not in center_dose_to_signatures.keys()
+        ):
+            raise ValueError(
+                f"Secondary dose offsets {secondary_dose} not found among center dose offsets {sorted(center_dose_to_signatures.keys())}"
+            )
+
+    dose_offsets_to_annotations = defaultdict(set)
+    offsets_and_type_to_index = {
+        offsets_and_type: idx
+        for idx, offsets_and_type in enumerate(
+            sorted(
+                set(
+                    chain(
+                        {(k, "central_dose") for k in center_dose_to_signatures.keys()},
+                        (
+                            (v, k)
+                            for _val in center_dose_to_signatures.values()
+                            for k, v in _val.items()
+                            if k not in {"dtr", "cuis", "secondary_dose"}
+                        ),
+                    )
+                )
+            )
+        )
+    }
+    for offsets in center_dose_to_signatures.keys():
+        start, end = offsets
+        dose_offsets_to_annotations[offsets].add(
+            rt_cell_to_ls_entity(
+                start=start,
+                end=end,
+                text=file_text[start:end],
+                rt_column_name="central_dose",
+                ls_id=f"{file_index}_rt_{offsets_and_type_to_index.get((offsets, 'central_dose'), 'ERROR')}",
+                from_name="text",
+                origin="prediction",
+            )
+        )
+    signature_pairs = set()
+    dose_dose_pairs = set()
+    signature_offsets_to_annotation = defaultdict(set)
+    for center_dose_offsets, value_dict in center_dose_to_signatures.items():
+        center_dose_id = f"{file_index}_rt_{offsets_and_type_to_index.get((center_dose_offsets, 'central_dose'), 'ERROR')}"
+        center_dose_start, center_dose_end = center_dose_offsets
+        secondary_dose = value_dict.get("secondary_dose")
+        if secondary_dose is not None:
+            if secondary_dose not in dose_offsets_to_annotations.keys():
+                raise ValueError(
+                    f"Secondary dose offsets {value_dict['secondary_dose']} not found among center dose offsets {sorted(dose_offsets_to_annotations.keys())}"
+                )
+            secondary_dose_id = f"{file_index}_rt_{offsets_and_type_to_index.get((secondary_dose, 'central_dose'), 'ERROR')}"
+            dose_dose_pairs.add(
+                (
+                    center_dose_id,
+                    secondary_dose_id,
+                )
+            )
+        if value_dict.get("dtr") is None:
+            raise ValueError(f"{center_dose_offsets} missing DTR")
+        dose_offsets_to_annotations[center_dose_offsets].add(
+            dtr_cell_to_ls_entity(
+                start=center_dose_start,
+                end=center_dose_end,
+                text=file_text[center_dose_start:center_dose_end],
+                dtr_labels=value_dict["dtr"],
+                ls_id=center_dose_id,
+                origin="prediction",
+            )
+        )
+
+        if value_dict.get("cuis") is None:
+            raise ValueError(f"{center_dose_offsets} missing CUIs")
+        dose_offsets_to_annotations[center_dose_offsets].add(
+            cui_cell_to_ls_entity(
+                start=center_dose_start,
+                end=center_dose_end,
+                cui_labels=value_dict["cuis"],
+                ls_id=center_dose_id,
+                origin="prediction",
+            )
+        )
+        for signature_name in value_dict.keys() - {
+            "dtr",
+            "cuis",
+            "secondary_dose",
+            "central_dose",
+        }:
+            signature_offsets = value_dict.get(signature_name)
+            if signature_offsets is not None:
+                signature_start, signature_end = signature_offsets
+                ls_id = f"{file_index}_rt_{offsets_and_type_to_index.get((signature_offsets, signature_name), 'ERROR')}"
+                ls_entity = rt_cell_to_ls_entity(
+                    start=signature_start,
+                    end=signature_end,
+                    text=file_text[signature_start:signature_end],
+                    rt_column_name=signature_name,
+                    ls_id=ls_id,
+                    from_name="text",
+                    origin="prediction",
+                )
+                signature_offsets_to_annotation[signature_offsets].add(ls_entity)
+                signature_pairs.add(
+                    (
+                        center_dose_id,
+                        ls_id,
+                    )
+                )
+    for v in dose_offsets_to_annotations.values():
+        assert len(v) == 3, v
+        for elem in v:
+            yield elem
+    for v in signature_offsets_to_annotation.values():
+        # assert len(v) == 1, v
+        for elem in v:
+            yield elem
+
+    def id_key(ls_id: str) -> int:
+        last = ls_id.split("_")[-1]
+        if not last.isnumeric():
+            raise ValueError(f"Bad LS ID: {ls_id}")
+        return int(last)
+
+    for signature_pair in signature_pairs:
+        anchor, signature = signature_pair
+        yield entities_to_relation(
+            from_id=anchor,
+            to_id=signature,
+            direction="right" if id_key(anchor) <= id_key(signature) else "left",
+        )
+
+    for dose_dose_pair in dose_dose_pairs:
+        anchor, signature = dose_dose_pair
+        yield entities_to_relation(
+            from_id=anchor,
+            to_id=signature,
+            labels=["SecondaryDose"],
+            direction="right" if id_key(anchor) <= id_key(signature) else "left",
+        )
 
 
 def rt_dict_to_ls_annotations(
